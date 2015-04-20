acca7c9580edbb0781e6838f258dae0c50505a47
moukku <mikpi@uef.fi>
2015-04-20 16:08:34 +0300

    general improvements

--- pickCartilage.m
+++ pickCartilage.m
@@ -1,19 +1,39 @@
-function [Position, BWimage] = pickCartilage(Image)
-                 
-                level = graythresh(Image);
-                Image = im2bw(Image,level);
+function [Position, BWimage] = pickCartilage(oImage)
+                Original = oImage;
+
+                %Treshold found by trial and error
+                oImage(oImage < 0.1) = .0;
+                level = graythresh(oImage);
+                Image = im2bw(oImage,level);
                 
                 %Get image Dimensions
                 [width, ~] = size(Image);
                 
                 %Remove small objects from image
-                Image = bwareaopen(Image, round(width * 0.2));
+                Image = bwareaopen(Image, round(width * 0.5));
                 
                 %Fill holes from image
                 Image = imfill(Image, 'holes');
                 
-                [x y] = ginput(1);
                 
+                NumOfElements = bwconncomp(Image);
+                %Erase the smallest element from the list untill there's only
+                %num connected elements left
+                if(NumOfElements.NumObjects > 2)
+                    Toomuch = true;
+                    while(Toomuch == true)
+                        numPixels = cellfun(@numel,NumOfElements.PixelIdxList);
+                        [~,idx] = min(numPixels);
+                        Image(NumOfElements.PixelIdxList{idx}) = 0;
+                        NumOfElements = bwconncomp(Image);
+                            if(NumOfElements.NumObjects == 2)
+                                Toomuch = false;
+                            end
+                    end
+                end
+                
+                imshow(Original + Image);
+                [x y] = ginput(1);
                 %Select the cartilage
                 BWimage = bwselect(Image, x, y);
                 Position = [x,y];
